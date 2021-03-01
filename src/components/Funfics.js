91e8583bc671f75afcc7011aab1fae235cfeb679
Artsiom Kashkou <a.kashkou@stud.elka.pw.edu.pl>
2021-03-01 13:08:53 +0300

    Add loading spinner

--- src/components/Funfics.js
+++ src/components/Funfics.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "../logo.svg";
 import {useHistory} from "react-router-dom";
+import {Spinner} from "react-bootstrap";
 
 const FUNFICS_URL = new URL('/funfics', process.env.REACT_APP_API_URL);
 
-async function funficsResponseText() {
+async function funficsResponseJson() {
   const response = await fetch(FUNFICS_URL.toString())
   if (response.ok) {
     return await response.json()
@@ -15,27 +15,35 @@ async function funficsResponseText() {
 
 export default function Funfics() {
   const [funfics, setFunfics] = React.useState([])
+  const [loadingFunfics, setLoadingFunfics] = React.useState(true)
   const history = useHistory();
 
   React.useEffect(() => {
-    (async () => {
-      const got = await funficsResponseText()
-      if (got) {
-        setFunfics(got)
-      }
-    })()
+    funficsResponseJson()
+      .then(value => {
+        setFunfics(value)
+        setLoadingFunfics(false)
+      })
   }, [])
 
   const readFunfic = id => history.push(`/read/${id}`)
 
+  const spinner = (
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading funfics...</span>
+    </Spinner>
+  )
+
+  const mappedFunfics = (
+    funfics.length
+      ? funfics.map(f => <p onClick={() => readFunfic(f.id)} key={f.id}>{`${f.id}: ${f.name}`}</p>)
+      : "There are no funfics in the database"
+  )
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        {funfics.length
-          ? funfics.map((f, ind) => <p onClick={() => readFunfic(f.id)} key={f.id}>{`${f.id}: ${f.name}`}</p>)
-          : "No funfics yet"
-        }
+        {loadingFunfics ? spinner : mappedFunfics}
       </header>
     </div>
   )
