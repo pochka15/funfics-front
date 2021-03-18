61fbd189e2945a67f7cdac6d3407f49b1d5ed8df
Artsiom Kashkou <a.kashkou@stud.elka.pw.edu.pl>
2021-03-18 22:14:12 +0300

    Implement User last login date

--- src/components/admin/UserManagement.js
+++ src/components/admin/UserManagement.js
@@ -8,14 +8,14 @@ import {AuthContext} from "../../contexts/AuthContext";
 import User from "../../dto/user";
 
 function UserManagement() {
-  const [users, setUsers] = useState(undefined)
+  const [tableUsers, setTableUsers] = useState(undefined)
   const auth = useContext(AuthContext)
 
   const reloadUsersData = useCallback(() => {
     if (auth.isAuthenticated) {
       receiveData(ApiUrls.ADMIN_USERS,
         defaultAuthConfig(auth.token),
-        (usersData) => setUsers(configured(usersData)),
+        (usersData) => setTableUsers(usersData.map(toUser).map(addRegistrationDate)),
         (error) => console.log(error.message))
     }
   }, [auth])
@@ -24,19 +24,26 @@ function UserManagement() {
     reloadUsersData();
   }, [auth, reloadUsersData])
 
-  function configured(users) {
-    return users.map(user => new User(
-      user.enabled ? "Enabled" : "Disabled",
+  function toUser(user) {
+    return new User(user.enabled ? "Enabled" : "Disabled",
       user.name,
       user.id,
-      user.registrationDate,
-      user.roles.join(", ")));
+      user.registrationIsoDateTime,
+      user.lastLoginIsoDateTime,
+      user.roles.join(", "))
+  }
+
+  function addRegistrationDate(user) {
+    return {
+      ...user,
+      registrationDate: user.registrationIsoDateTime.split("T")[0]
+    }
   }
 
   return (
     <Container>
-      {users
-        ? <UsersTable reloadUsers={reloadUsersData} users={users}/>
+      {tableUsers
+        ? <UsersTable reloadUsers={reloadUsersData} users={tableUsers}/>
         : <div className="Centered"><CustomSpinner/></div>}
     </Container>
   );
